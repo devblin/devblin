b4fd583228d9c01b3628d7688537aeb4b3eeba1e
-h <@users.noreply.github.com>
2021-05-16 00:22:49 +0530

    updated quote area

--- update.php
+++ update.php
@@ -32,14 +32,14 @@
     //Readme data to update
     $hello = "# Hello 👋\n\n";
     $recent = "🌱 In pandemic. <br> ⚡ Fun fact: Lazy.\n\n";
-    $quote_area = "<div align='center'><i>\"$quote_content\"</i><h6> - $quote_author</h6></div>\n\n";
-    $center_align_start = "<p align=\"center\">\n";
-    $github_stats = "<img src=\"https://github-readme-stats.vercel.app/api?username=devblin&count_private=true&show_icons=true&hide_border=true&bg_color=00000000&text_color=3790D7&title_color=FF2D2D&icon_color=fb8c00&include_all_commits=true&custom_title=📙 Deepanshu Dhruw's Github Stats\">\n";
-    $most_lang = "<img src=\"https://github-readme-stats.vercel.app/api/top-langs/?username=devblin&layout=compact&hide=&langs_count=10&hide_border=true&bg_color=00000000&text_color=3790D7&title_color=FF2D2D&icon_color=fb8c00&custom_title=💻 Most Used Languages\">\n";
-    $streak_stats = "<img src=\"https://github-readme-streak-stats.herokuapp.com?user=devblin&theme=dark&hide_border=true&background=00000000&stroke=FF2D2D&ring=FF2D2D&currStreakLabel=3790D7&dates=3790D7&currStreakNum=3790D7&sideNums=3790D7&sideLabels=3790D7\">\n";
-    $wakatime = "<img src=\"https://github-readme-stats.vercel.app/api/wakatime?username=devblin&layout=compact&theme=dark&hide_border=true&bg_color=00000000&text_color=3790D7&title_color=FF2D2D&custom_title=⏳ Wakatime Stats\">\n";
+    $quote_area = "<div align=\"center\"><b>\"$quote_content\"</b><br><i> - $quote_author</i></div>\n\n";
+    $center_align_start = "<p align=\"center\">\n"; 
+    $github_stats = "\t<img src=\"https://github-readme-stats.vercel.app/api?username=devblin&count_private=true&show_icons=true&hide_border=true&bg_color=00000000&text_color=3790D7&title_color=FF2D2D&icon_color=fb8c00&include_all_commits=true&custom_title=📙 Deepanshu Dhruw's Github Stats\">\n";
+    $most_lang = "\t<img src=\"https://github-readme-stats.vercel.app/api/top-langs/?username=devblin&layout=compact&hide=&langs_count=10&hide_border=true&bg_color=00000000&text_color=3790D7&title_color=FF2D2D&icon_color=fb8c00&custom_title=💻 Most Used Languages\">\n";
+    $streak_stats = "\t<img src=\"https://github-readme-streak-stats.herokuapp.com?user=devblin&theme=dark&hide_border=true&background=00000000&stroke=FF2D2D&ring=FF2D2D&currStreakLabel=3790D7&dates=3790D7&currStreakNum=3790D7&sideNums=3790D7&sideLabels=3790D7\">\n";
+    $wakatime = "\t<img src=\"https://github-readme-stats.vercel.app/api/wakatime?username=devblin&layout=compact&theme=dark&hide_border=true&bg_color=00000000&text_color=3790D7&title_color=FF2D2D&custom_title=⏳ Wakatime Stats\">\n";
     $center_align_end = "</p>\n\n";
-    $last_updated = "<h6>Last updated: $date</h6>";
+    $last_updated = "<kbd>Last updated: $date</kbd>";
 
     //Read README.md
     $readme = fopen("README.md", "w");
