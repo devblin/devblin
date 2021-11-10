8a8775c2e85da612b02d39d57163bebc383bf961
Deepanshu Dhruw <deepanshu.dhruw@outlook.com>
2021-11-10 22:46:35 +0530

    Update update.php

--- update.php
+++ update.php
@@ -38,7 +38,7 @@
     $github_stats = "\t<img src=\"https://github-readme-stats.vercel.app/api?username=devblin&count_private=true&show_icons=true&hide_border=true&bg_color=00000000&text_color=3790D7&title_color=FF2D2D&icon_color=fb8c00&include_all_commits=true&custom_title=📙 Deepanshu Dhruw's Github Stats\">\n";
     $most_lang = "\t<img src=\"https://github-readme-stats.vercel.app/api/top-langs/?username=devblin&layout=compact&hide=&langs_count=10&hide_border=true&bg_color=00000000&text_color=3790D7&title_color=FF2D2D&icon_color=fb8c00&custom_title=💻 Most Used Languages\">\n";
     $streak_stats = "\t<img src=\"https://github-readme-streak-stats.herokuapp.com?user=devblin&theme=dark&hide_border=true&background=00000000&stroke=FF2D2D&ring=FF2D2D&currStreakLabel=3790D7&dates=3790D7&currStreakNum=3790D7&sideNums=3790D7&sideLabels=3790D7\">\n";
-    $wakatime = "\t<img src=\"https://github-readme-stats.vercel.app/api/wakatime?username=devblin&layout=compact&theme=dark&hide_border=true&bg_color=00000000&text_color=3790D7&title_color=FF2D2D&custom_title=⏳ Wakatime Stats\">\n";
+    $wakatime = "\t<img src=\"https://github-readme-stats.vercel.app/api/wakatime?username=devblin&layout=compact&theme=dark&hide_border=true&bg_color=00000000&text_color=3790D7&title_color=FF2D2D&langs_count=20&custom_title=⏳ Wakatime Stats\">\n";
     $center_align_end = "</p>\n\n";
     $last_updated = "<kbd>Last updated: $date</kbd>";
 
